6a263e997e17cefb0338d1ab08c2823b8a92b6ff
janthony25 <junanthony.business@gmail.com>
2024-02-04 11:18:19 +1300

    Add project files.

---
+++ OOP-Method-Overriding2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method overriding -  Practice
+
+            Jun jun = new Jun();
+            Lim lim = new Lim();
+
+            jun.selfIntroduce();
+            lim.selfIntroduce();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Introduce
+    {
+        public virtual void selfIntroduce()
+        {
+            Console.WriteLine("Hi I am this person");
+        }
+    }
+
+    class Jun : Introduce
+    {
+        public override void selfIntroduce()
+        {
+            Console.WriteLine("Hi, my name is Jun and I am 24 years old");
+        }
+    }
+
+    class Lim : Introduce
+    {
+        public override void selfIntroduce()
+        {
+            Console.WriteLine("Hi, my name is Lim and I am 21 years old");
+        }
+    }
+
+
+}
